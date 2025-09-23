93888d913c5bbf87f271b0def3627a81a3685664
renatoargh <renatoargh@gmail.com>
2025-09-22 21:11:12 -0300

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,27 +5,12 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: (origin, callback) => {
-        if (!origin) {
-          return callback(null, true);
-        }
-        
-        const allowedOrigins = [
-          'http://localhost:8080',
-          'https://www.consultorfamiliar.com.br',
-          'https://consultorfamiliar.com.br',
-        ];
-        
-        if (allowedOrigins.includes(origin)) {
-          return callback(null, true);
-        }
-        
-        if (origin.match(/^https:\/\/[a-zA-Z0-9\-\.]+\.lovable\.app$/)) {
-          return callback(null, true);
-        }
-        
-        return callback(new Error('Not allowed by CORS'));
-      },
+      origin: [
+        'http://localhost:8080',
+        'https://www.consultorfamiliar.com.br',
+        'https://consultorfamiliar.com.br',
+        'https://family-banker-home.lovable.app'
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
